341c4aafc3f4a6d4798f90d9b31457065eb4d6b8
jds8 <jds464@cornell.edu>
2021-10-09 20:54:56 -0700

    zoterooo

--- packages.el
+++ packages.el
@@ -24,6 +24,9 @@
 (package! taxy
   :recipe (:host github :repo "alphapapa/taxy.el"))
 
+(package! zotxt
+  :recipe (:host github :repo "egh/zotxt-emacs"))
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
