382f26bfbf657495b435ddc281d71550c9368ccb
jds8 <jds464@cornell.edu>
2022-01-28 07:19:31 -0800

    org inline image

--- config.el
+++ config.el
@@ -495,3 +495,33 @@
 ;; (setq org-mind-map-engine "sfdp")   ; Multiscale version of fdp for the layout of large graphs
 ;; (setq org-mind-map-engine "twopi")  ; Radial layouts
 ;; (setq org-mind-map-engine "circo")  ; Circular Layout
+;; Copyright (C) 2014 Matus Goljer <matus.goljer@gmail.com>
+;; Package-requires: ((dash "2.5.0"))
+
+;; org-inline-image
+(defun org-inline-image--get-current-image ()
+  "Return the overlay associated with the image under point."
+  (car (--select (eq (overlay-get it 'org-image-overlay) t) (overlays-at (point)))))
+
+(defun org-inline-image--get (prop)
+  "Return the value of property PROP for image under point."
+  (let ((image (org-inline-image--get-current-image)))
+    (when image
+      (overlay-get image prop))))
+
+(defun org-inline-image-animate ()
+  "Animate the image if it's possible."
+  (interactive)
+  (let ((image-props (org-inline-image--get 'display)))
+    (when (image-multi-frame-p image-props)
+      (image-animate image-props))))
+
+(defun org-inline-image-animate-auto ()
+  (interactive)
+  (when (eq 'org-mode major-mode)
+    (while-no-input
+      (run-with-idle-timer 0.3 nil 'org-inline-image-animate))))
+
+(setq org-inline-image--get-current-image (byte-compile 'org-inline-image--get-current-image))
+(setq org-inline-image-animate  (byte-compile 'org-inline-image-animate ))
+(add-hook 'post-command-hook 'org-inline-image-animate-auto)
